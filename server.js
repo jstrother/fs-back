388976cbd9a850bd79b84765d1ce015bde3c0376
jstrother <jim.strother@gmail.com>
2024-12-05 21:18:06 -0600

    Fixed error with cors package

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-// import cors from 'cors';
+import cors from 'cors';
 import connectDB from './database/connection.js';
 import userRouter from './routes/fantasy/userRouter.js';
 
@@ -12,8 +12,13 @@ const jsonParser = bodyParser.json();
 connectDB();
 
 app.use(jsonParser);
-// app.use(cors);
+app.use(cors());
+app.use(express.static('public'));
 app.use('/user', userRouter);
+
+app.get('/', (req, res) => {
+  res.send('Fantasy Soccer for All!');
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
