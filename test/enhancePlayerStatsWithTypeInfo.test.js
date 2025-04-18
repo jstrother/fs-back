97338adb18dd17b993911423f77b574d7359f896
jstrother <jim.strother@gmail.com>
2025-04-17 21:41:05 -0500

    removed test/ until a better package than jest can be found -- don't want to use babel on my back end

--- test/enhancePlayerStatsWithTypeInfo.test.js
+++
@@ -1,5 +0,0 @@
-import mongoose from 'mongoose';
-import enhancePlayerStatsWithTypeInfo from '../statistics/players/enhancePlayerStatsWithTypeInfo.js';
-import Type from '../schema/api/typeSchema.js';
-
-jest.mock('../utils/logger.js')
